03afa15631a70ab1b2067f10b7b78a40d10047e6
lwyers1@hotmail.co.uk <lwyers1@hotmail.co.uk>
2023-07-07 10:45:39 +0100

    getEmployerRole.js

--- src/kata7.getEmployerRole.js
+++ src/kata7.getEmployerRole.js
@@ -1,13 +1,10 @@
-
 function getEmployerRole(employeeName, employees) {
-const employee = employees.find((emp) => emp.name ===employeeName);
-
-if (employee) {
+  const employee = employees.find((emp) => emp.name === employeeName);
+  
+  if (employee) {
     return employee.role;
+  }
+  return "Employee not found";
 }
-return "Employee not found";
-}
-
-
 
 module.exports = getEmployerRole;
