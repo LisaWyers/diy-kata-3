78194ee227b39725eafa141b3d3e798cd70b0804
lwyers1@hotmail.co.uk <lwyers1@hotmail.co.uk>
2023-07-07 10:03:26 +0100

    kata2.booleanToWord.test.js

--- test/kata2.booleanToWord.test.js
+++ test/kata2.booleanToWord.test.js
@@ -1,8 +1,8 @@
 const { booleanToWord } = require("../src");
 
 describe("booleanToWord", () => {
-it('returns yes when passed to true', () => {
-    expect(booleanToWord(true)).toBe('yes')
-    expect(booleanToWord(false)).toBe('no')
-})
+  it("returns yes when passed to true", () => {
+    expect(booleanToWord(true)).toBe("yes");
+    expect(booleanToWord(false)).toBe("no");
+  });
 });
