582a530ffc111a4097dacdcb2c075fc632184d23
lwyers1@hotmail.co.uk <lwyers1@hotmail.co.uk>
2023-07-07 10:33:52 +0100

    humanCatDogYears.js

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,22 +1,18 @@
-const humanCatDogYears = (humanYears) => {;
-    let catYears, dogYears;
+const humanCatDogYears = (number) => {
+  let catYears, dogYears;
 
-    if (humanYears === 1) {
-      catYears = 15;
-      dogYears = 15;
-    } 
+  if (number === 0) {
+    catYears = 0;
+    dogYears = 0;
+  } else if (humanYears === 1) {
+    catYears = 15;
+    dogYears = 15;
+  } else {
+    catYears = 24 + (humanYears + 4);
+    dogYears += 5 + (humanYears + 4);
+  }
 
-    else if (humanYears === 2) {
-      catYears = 24;
-      dogYears = 24;
-    } 
-
-    else {
-      catYears = 24 + (humanYears +4);
-      dogYears += 5 + (humanYears +4);
-    }
-    
-    return [humanYears, catYears, dogYears];
+  return [humanYears, catYears, dogYears];
 };
 
-module.exports = humanCatDogYears
+module.exports = humanCatDogYears;
