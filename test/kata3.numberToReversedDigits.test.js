6b74a33b986083a45fb61f7b7ed9a9f3f53d7a4f
lwyers1@hotmail.co.uk <lwyers1@hotmail.co.uk>
2023-07-07 10:10:50 +0100

    numberToReversedDigits.test.js

--- test/kata3.numberToReversedDigits.test.js
+++ test/kata3.numberToReversedDigits.test.js
@@ -1,7 +1,9 @@
 const { numberToReversedDigits } = require("../src");
 
 describe("numberToReversedDigits", () => {
-  it ("returns a reversed array of the number's digits", () => {
-    expect (numberToReversedDigits([12345])).toStrictEqual([5, 4, 3, 2, 1])
+  it("returns a reversed array of the number's digits", () => {
+    expect(numberToReversedDigits([12345])).toStrictEqual([5, 4, 3, 2, 1]);
+    expect(numberToReversedDigits([87654321])).toStrictEqual([1, 2, 3, 4, 5, 6, 7, 8]);
+    expect(numberToReversedDigits([456])).toStrictEqual([6, 5, 4]);
   });
 });
